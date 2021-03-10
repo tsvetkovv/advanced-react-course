c3f330723a3db2951fa3673fbbbd18ad037193fa
Viktor Tsvetkov <11594423+Tsvetkovv@users.noreply.github.com>
2021-03-10 19:35:49 +0100

    add progress bar

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import PropTypes from 'prop-types';
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
