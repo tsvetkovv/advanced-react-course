7b11f5091fbb75a40d18d2bfb03d04a1bd281acf
Viktor Tsvetkov <11594423+Tsvetkovv@users.noreply.github.com>
2021-03-10 20:44:02 +0100

    add base keystone config

---
+++ backend/keystone.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  statelessSessions,
+  withItemData,
+} from '@keystone-next/keystone/session';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('No DATABASE_URL');
+}
+
+if (!process.env.COOKIE_SECRET) {
+  throw new Error('No COOKIE_SECRET');
+}
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  session: withItemData(statelessSessions(sessionConfig)),
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+  },
+  lists: createSchema({}),
+  ui: {
+    isAccessAllowed: (context) => true,
+  },
+});
