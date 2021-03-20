0ed8e2666ae5059c6b121f66c36c1de46b13fa7f
Viktor Tsvetkov <11594423+Tsvetkovv@users.noreply.github.com>
2021-03-20 19:51:20 +0100

    apply nav styles

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 export default function Nav() {
   // noinspection HtmlUnknownTarget
   return (
-    <nav>
+    <NavStyles>
       <Link href="/products">Products</Link>
       <Link href="/sell">Sell</Link>
       <Link href="/orders">Orders</Link>
       <Link href="/account">Account</Link>
-    </nav>
+    </NavStyles>
   );
 }
