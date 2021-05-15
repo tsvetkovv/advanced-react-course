ca32b456c499695e54cacba0ce9a22e7607e2339
Viktor Tsvetkov <11594423+Tsvetkovv@users.noreply.github.com>
2021-05-15 16:53:17 +0200

    add pagination to products page

---
+++ frontend/components/Pagination.tsx
@@ -0,0 +1,58 @@
+import PropTypes from "prop-types";
+import Head from "next/head";
+import Link from "next/link";
+
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/client";
+import PaginationStyles from "./styles/PaginationStyles";
+import DisplayError from "./ErrorMessage";
+import { perPage } from "../config";
+
+interface PaginationQueryData {
+  meta: {
+    count: number;
+  };
+}
+
+export const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    meta: _allProductsMeta {
+      count
+    }
+  }
+`;
+
+function Pagination({ page }: { page: number }) {
+  const { error, loading, data } = useQuery<PaginationQueryData>(
+    PAGINATION_QUERY
+  );
+  if (loading) return <p>Loading...</p>;
+  if (error || !data) return <DisplayError error={error} />;
+  const total = data.meta.count;
+  const pageCount = Math.ceil(total / perPage);
+  return (
+    <PaginationStyles>
+      <Head>
+        <title>Sick Fits - Page {page} of ____</title>
+      </Head>
+      <Link href={`/products/${page - 1}`}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a aria-disabled={page === 1}>👈 Prev</a>
+      </Link>
+      <p>
+        Page {page} of {pageCount}
+      </p>
+      <p>{total} Items Total</p>
+      <Link href={`/products/${page + 1}`}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a aria-disabled={page >= pageCount}>Next 👉</a>
+      </Link>
+    </PaginationStyles>
+  );
+}
+
+Pagination.propTypes = {
+  page: PropTypes.number.isRequired,
+};
+
+export default Pagination;
